92b8dba6166782e98c0aba1f4a019eb2b6ff31e0
Andrii <kononov.muma@gmail.com>
2024-02-19 09:25:42 +0200

    services/cloudinary, qr

---
+++ src/services/cloudinary.py
@@ -0,0 +1,72 @@
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Import to format the JSON responses
+# ==============================
+import json
+
+# Set configuration parameter: return "https" URLs by setting secure=True
+# ==============================
+config = cloudinary.config(secure=True)
+
+# Log the configuration
+# Copy this URL in a browser tab to generate the image on the fly.
+# ==============================
+print("****1. Set up and configure the SDK:****\n", config.cloud_name, config.api_key, "\n")
+
+
+def uploadImage():
+    # Upload the image and get its URL
+    # ==============================
+
+    # Upload the image.
+    # Set the asset's public ID and allow overwriting the asset with new versions
+    cloudinary.uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/butterfly.jpeg",
+                               public_id="quickstart_butterfly", unique_filename=False, overwrite=True)
+
+    # Build the URL for the image and save it in the variable 'srcURL'.
+    srcURL = cloudinary.CloudinaryImage("quickstart_butterfly").build_url()
+
+    # Log the image URL to the console.
+    print("****2. Upload an image****\nDelivery URL: ", srcURL, "\n")
+
+
+def getAssetInfo():
+    # Get and use details of the image
+    # ==============================
+
+    # Get image details and save it in the variable 'image_info'.
+    image_info = cloudinary.api.resource("quickstart_butterfly")
+    print("****3. Get and use details of the image****\nUpload response:\n", json.dumps(image_info, indent=2), "\n")
+
+    # Assign tags to the uploaded image based on its width. Save the response to the update in the variable 'update_resp'.
+    if image_info["width"] > 900:
+        update_resp = cloudinary.api.update("quickstart_butterfly", tags="large")
+    elif image_info["width"] > 500:
+        update_resp = cloudinary.api.update("quickstart_butterfly", tags="medium")
+    else:
+        update_resp = cloudinary.api.update("quickstart_butterfly", tags="small")
+
+    # Log the new tag to the console.
+    print("New tag: ", update_resp["tags"], "\n")
+
+
+def createImageTag():
+    # Transform the image
+    # ==============================
+
+    # Create an image tag with transformations applied to the src URL.
+    imageTag = cloudinary.CloudinaryImage("quickstart_butterfly").image(radius="max", effect="sepia")
+
+    # Log the image tag to the console
+    print("****4. Transform the image****\nImage Tag: ", imageTag, "\n")
+
+
+def main():
+    uploadImage()
+    getAssetInfo()
+    createImageTag()
+
+
+main();
---
+++ src/services/qr.py
@@ -0,0 +1,23 @@
+import qrcode
+
+# Функція для генерації QR-коду з URL
+def generate_qr_code(url, file_path):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Зберегти QR-код у зазначений файл
+    img.save(file_path)
+
+# Приклад виклику функції з URL і шляхом до файлу
+url_to_encode = "https://www.example.com"
+output_file_path = "qr_code.png"
+
+generate_qr_code(url_to_encode, output_file_path)
