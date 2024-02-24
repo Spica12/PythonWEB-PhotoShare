5e1c2a8d9be74e3fd650e406774edb43020b3f7d
Artificer-ua <metal.work3r@gmail.com>
2024-02-24 21:56:16 +0200

    Adding logout feature, refactor some auth functions. Tokens: add to blacklist when logout, check in get_current_user if it is in blacklist, delete refresh token from db for current user when logout.

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,3 +1,4 @@
+from copy import copy
 from datetime import datetime, timedelta
 from typing import Optional
 from fastapi import Depends, HTTPException, status
@@ -21,6 +22,12 @@ class AuthService:
     ALGORITHM = config.ALGORITHM
 
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=messages.COULD_NOT_VALIDATE_CREDENTIALS,
+        headers={"WWW-AUTHENTICATE": "BEARER"},
+    )
 
     async def get_user_by_id(self, user_id: UUID, db: AsyncSession):
         user = await UserRepo(db).get_user_by_id(user_id)
@@ -49,39 +56,42 @@ class AuthService:
     #     user = await UserRepo(db).get_user_by_email(email)
     #     return user
 
-    async def get_current_user(
-        self, token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
-    ):
-        credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=messages.COULD_NOT_VALIDATE_CREDENTIALS,
-            headers={"WWW-AUTHENTICATE": "BEARER"},
-        )
+    async def extract_token_data(self, token, db: AsyncSession):
+        # double usage of code, separate func
+        # check if we can use token (not in blacklist)
+        # if it is - raise 401.
+        blacklist = await UserRepo(db).get_token_blacklist(token)
+        if blacklist is not None:
+            raise self.credentials_exception
+
         try:
             payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
             email = payload["sub"]
-
             if payload["scope"] == "access_token":
                 if email is None:
-                    raise credentials_exception
+                    raise self.credentials_exception
             else:
-                raise credentials_exception
-
-        except JWTError as e:
-            raise credentials_exception
-
+                raise self.credentials_exception
+        except JWTError:
+            raise self.credentials_exception
         user_hash = str(email)
         user = await UserRepo(db).get_user_by_email(user_hash)
         if user is None:
-            raise credentials_exception
-
+            raise self.credentials_exception
         return user
-        # to router need add next
-        #
-        # from src.models.users import UserModel
-        # from src.services.auth import auth_service
-        #
-        # user: UserModel = Depends(auth_service.get_current_user)
+
+    async def get_current_user(self, token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+        """
+        need add to routers:
+
+        from src.models.users import UserModel
+        from src.services.auth import auth_service
+
+        user: UserModel = Depends(auth_service.get_current_user)
+        """
+        # if token not in blacklist and user exists - extract and return current user
+        user = await self.extract_token_data(token, db)
+        return user
 
     async def create_access_token(self, email: str, expires_delta: Optional[float] = None):
         to_encode = {"sub": str(email)}
@@ -106,7 +116,6 @@ class AuthService:
             {"iat": datetime.utcnow(), "exp": expire, "scope": "refresh_token"}
         )
         encode_jwt = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
-
         return encode_jwt
 
     async def get_refresh_token_by_user(self, user: UserModel, db: AsyncSession):
@@ -116,7 +125,7 @@ class AuthService:
     async def update_refresh_token(self, user: UserModel, refresh_token: str | None, db: AsyncSession):
         await UserRepo(db).update_refresh_token(user, refresh_token)
 
-    async def add_token_to_blacklist(self, user: UserModel, token: str, db: AsyncSession):
+    async def add_token_to_blacklist(self, token: str, db: AsyncSession):
         await UserRepo(db).add_token_to_blacklist(token)
 
     def create_email_token(self, data: dict):
@@ -138,6 +147,19 @@ class AuthService:
 
     async def confirmed_email(self, user: UserModel, db: AsyncSession):
         await UserRepo(db).confirmed_email(user)
+
+    async def logout_service(self, token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+        # extract user from token
+        # if we have no user
+        # using copy because of sqlalchemy rewrite some data in previous objects
+        user = copy(await self.extract_token_data(token, db))
+
+        # clear refresh token data for current user
+        await UserRepo(db).remove_refresh_token(user)
+
+        # adding access token to blacklist
+        await self.add_token_to_blacklist(token=token, db=db)
+        return "logout"
 
 
 auth_service = AuthService()
