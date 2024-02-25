c35771352bf5f4a355c24651b7f7cc2daf3f8966
Artificer-ua <metal.work3r@gmail.com>
2024-02-25 18:49:39 +0200

    Rewrite schemas

--- src/schemas/unified.py
+++ src/schemas/unified.py
@@ -1,25 +1,18 @@
 from typing import List, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 # schemas
 from src.schemas.users import UserNameSchema
-from src.schemas.photos import ImageUpdateSchema
+from src.schemas import photos
 from src.schemas.rating import RateSchema
 from src.schemas.comment import CommentResponseIntegratedSchema
 
 
-# todo -> ImageSchema
-# todo add tags
-class ImagePageResponseShortSchema(ImageUpdateSchema):
-    # tags: Optional[List[str]] = []
-    id: int
-    image_url: Optional[str]
+class ImagePageResponseShortSchema(photos.ImageResponseTagsSchema):
     username: Optional[str]#Optional[UserNameSchema]
     value: Optional[int] #Optional[RateSchema]
 
 
-
-# todo add tags
 class ImagePageResponseFullSchema(ImagePageResponseShortSchema):
     comments: Optional[List[CommentResponseIntegratedSchema]] = []
